19256ae7e7f4c3dc53525d4aa69d6fe80a6a52ff
eliranbarashi <snakepitdude@gmail.com>
2024-03-10 05:21:37 +0200

    added try/expect to toggle secret PASSWORD between local and remote

--- conftest.py
+++ conftest.py
@@ -5,7 +5,12 @@ import time
 import pytest
 from playwright.sync_api import Playwright
 
-# import utils.secret_config
+
+try:
+    PASSWORD = os.environ['PASSWORD']
+except KeyError:
+    import utils.secret_config
+    PASSWORD = utils.secret_config.PASSWORD
 
 
 @pytest.fixture(scope="session")
@@ -34,7 +39,7 @@ def login_set_up(set_up):
     page.get_by_test_id("emailAuth").get_by_label("Email").fill("symon.storozhenko@gmail.com")
     page.get_by_test_id("emailAuth").get_by_label("Email").press("Tab")
     # page.get_by_label("Password").fill(utils.secret_config.PASSWORD)
-    page.get_by_label("Password").fill(os.environ['PASSWORD'])
+    page.get_by_label("Password").fill(PASSWORD)
     page.get_by_label("Password").press("Enter")
 
     yield page
